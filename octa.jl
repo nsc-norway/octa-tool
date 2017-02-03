53a1a8c9c6dcac76a32f1ff746d6242fd35f4fef
Marius Bjørnstad <pmb@fa2k.net>
2017-02-03 10:43:26 +0100

    Initial analysis code

--- octa.jl
+++ octa.jl
@@ -1,18 +1,5 @@
 using LibExpat
 
-function get_dm_summary_paths(stats_dir)
-    stat_files = readdir(stats_dir)
-    demux_summary_files = Dict()
-    for filename = stat_files
-        m = match(r"^DemuxSummaryF1L(\d)\.txt", filename)
-        if (m != nothing)
-            demux_summary_files[parse(Int, m.captures[1])] =
-                    joinpath(stats_dir, filename)
-        end
-    end
-    return demux_summary_files
-end
-
 function get_unknown_barcodes(stats_dir, lane)
     summary_path = joinpath(stats_dir, "DemuxSummaryF1L$(lane).txt")
     unknown_barcode_count = Dict()
@@ -21,9 +8,8 @@ function get_unknown_barcodes(stats_dir, lane)
         for line = eachline(f)
             if found_unknown_bc_section
                 if ! startswith(line, "###")
-                    barcode = "Lol"
-                    count = 1
-                    unknown_barcode_count[barcode] = count
+                    (barcode, count_str) = split(line, "\t")
+                    unknown_barcode_count[barcode] = count_str
                 end
             elseif strip(line) == "### Most Popular Unknown Index Sequences"
                 found_unknown_bc_section = true
@@ -53,8 +39,40 @@ function get_demultiplexing_stats(stats_dir)
     return read_counts, perfect_read_counts
 end
 
-function analyse(sample_barcodes, unknown_barcodes)
-    # TODO analysis
+function analyse(sample_indexes, unknown_indexes, unknown_total_reads)
+    # Generates array with index1s and index2s as elements
+    sample_single_indexes = map(Set, zip(keys(sample_indexes)...))
+
+    combinations = [(i1, i2)
+            for i1 in sample_single_indexes[1],
+            i2 in sample_single_indexes[2]
+            ] 
+    feasible = length(combinations) > length(sample_indexes)
+    if ! feasible
+        return nothing
+    end
+
+    # Deal with index1 and index2 the same way, hold one fixed and test
+    # the other
+    in_known_samples = 0
+    in_misassigned = 0
+    for (fixed_i, test_i) = [(1,2), (2,1)]
+        # Process all sample indexes one by one
+        for index_seq = sample_single_indexes[fixed_i]
+            # Compute the number of known, unknown reads for this fixed
+            # single index
+            for other_index_seq = sample_single_indexes[test_i]
+                if fixed_i == 1
+                    key = [index_seq, other_index_seq]
+                else
+                    key = [other_index_seq, index_seq]
+                end
+                in_known_samples += get(sample_indexes, key, 0)
+                in_misassigned += get(unknown_indexes, key, 0)
+            end
+        end
+    end
+    return in_misassigned, in_known_samples
 end
 
 
@@ -83,14 +101,26 @@ function main(path)
     lanes = Set(map(coordinates -> coordinates[1], keys(read_counts)))
 
     for lane = lanes
+        result = nothing
         if in(lane, lanes_with_dual_indexing)
-            unknown_barcodes = get_unknown_barcodes(stats_dir, lane)
+            unknown_barcodes_raw = get_unknown_barcodes(stats_dir, lane)
+            unknown_barcodes = Dict(
+                        map(
+                            x -> split(replace(x[1], "+", "-"), "-") => x[2],
+                            unknown_barcodes_raw
+                            )
+                    )
+            filtered_read_counts = filter((k, v) ->
+                            (k[1] == lane && k[2] != "unknown"), read_counts)
             lane_read_counts = Dict(
-                [replace(barcode, "+", "-") => read_count
-                    for ((lane,barcode), read_count) in read_counts]
+                [split(replace(barcode, "+", "-"), "-") => read_count
+                    for ((_,barcode), read_count) in filtered_read_counts]
                 )
-            analyse(lane_read_counts, unknown_barcodes)
-            println(lane, "\t?\tNA")
+            unknown_total_reads = read_counts[(lane, "unknown")]
+            result = analyse(lane_read_counts, unknown_barcodes, unknown_total_reads)
+        end
+        if result != nothing
+            println(lane, "RESULT: ", result)
         else
             println(lane, "\tNA\tNA")
         end
